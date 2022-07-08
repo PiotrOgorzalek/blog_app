950218d553f7234be3de63d4a3b6ea91c229b481
piotrogorzalek <piotr.ogorzalek@ember.ltd>
2022-07-08 06:58:35 +0100

    start

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+    
+@section('content')
+    <body>
+        <article> 
+          <h1> 
+            {{ $post->title }}
+          </h1>
+
+          <div>   
+            {!! $post->body !!}
+          </div>
+
+        </article>
+
+        <a href="/"> Back </a>
+    </body>
+@endsection
